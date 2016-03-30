b2a056d4116411b3fed6353cc364ca73fb9fcbd6
rsdu <duruishan@yeah.net>
2016-03-30 12:18:47 +0800

    添加listitem_kindergarten

--- app/src/main/java/com/durs/photoview/MainActivity.java
+++ app/src/main/java/com/durs/photoview/MainActivity.java
@@ -1,16 +1,28 @@
 package com.durs.photoview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lv_kindergarten;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        registerListener();
     }
+
+    private void init() {
+        lv_kindergarten = (ListView)findViewById(R.id.lv_kindergarten);
+    }
+
+    private void registerListener(){
+
+    }
+
 
 }
